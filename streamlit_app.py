30cb88582b0f204d31b5393740ecaa71059ee33e
divyapatil3010557 <divya2003patil@gmail.com>
2026-05-20 17:20:57 +0530

    update streamlit_app.py
    Removed duplicate code for fetching and displaying smoothiefroot nutrition information.

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,6 +22,9 @@ if ingredients_list:
 
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+        sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
+        
 
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients, name_on_order)
                     values ('""" + ingredients_string + """','""" + name_on_order + """')"""
@@ -32,9 +35,6 @@ if ingredients_list:
         session.sql(my_insert_stmt).collect()
         st.success('Your Smoothie is ordered, ' + name_on_order + '!', icon="✅")
 
-# New section to display smoothiefroot nutrition information
-# New section to display smoothiefroot nutrition information
-import requests
-smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
+
+
 
